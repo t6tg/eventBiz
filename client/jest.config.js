22d393cae166207d4f66f7b8dc3f0011c422ea3d
thanawatgulati <thanawatgulatijames@gmail.com>
2020-09-15 00:23:47 +0700

    Change to NuxtJS

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,4 +1,21 @@
 module.exports = {
-  setupFiles: ["<rootDir>/enzyme.config.js"],
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-};
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  moduleFileExtensions: [
+    'js',
+    'vue',
+    'json'
+  ],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue'
+  ]
+}
